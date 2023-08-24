4400b9b2da4ed3e2d9517487da68d7565dbbca13
FavriDev98 <favripn@gmail.com>
2023-08-24 16:38:41 -0300

    tocadito

---
+++ js/almacenar.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let limpiar = document.getElementById('limpiar');
+    let lista = document.getElementById('contenedor')
+    limpiar.addEventListener('click', function() {
+        localStorage.clear();
+        lista.innerHTML = ``
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    let agregar = document.getElementById('agregar');
+    let arr = [];
+    let il = document.createElement('il');
+    let lista = document.getElementById('contenedor');
+    agregar.addEventListener('click', function() {
+        let item = document.getElementById('item').value;
+        arr.push(item);
+        localStorage.setItem("datos", arr);
+            for (let i = 0; i < arr.length; i++)
+            il.innerHTML = `
+                ${arr[i]} 
+            `;
+            lista.appendChild(il);
+    })
+})
