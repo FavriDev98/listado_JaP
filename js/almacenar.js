bfa2ef31d8cfaa3ca8539c0ef829c893cfd0d7bb
teageto <turnablees@gmail.com>
2023-08-29 00:04:59 -0300

    when press the enter button add a new item

--- js/almacenar.js
+++ js/almacenar.js
@@ -1,37 +1,49 @@
 let arr = [];
 document.addEventListener("DOMContentLoaded", function() {
     let limpiar = document.getElementById('limpiar');
-    let lista = document.getElementById('contenedor')
+    let lista = document.getElementById('contenedor');
     limpiar.addEventListener('click', function() {
         localStorage.clear();
-        lista.innerHTML = ``
+        lista.innerHTML = ``;
         arr = [];
-    })
-})
+    });
+});
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function(){
 
     let agregar = document.getElementById('agregar');
     const lista = document.getElementById('contenedor');
     
-        agregar.addEventListener('click', function addkey() {
-            lista.innerHTML = ``
+        agregar.addEventListener('click', function() {
+            lista.innerHTML = ``;
             let item = document.getElementById('item').value;
             arr.push(item);
-            localStorage.setItem("datos", arr);
+            localStorage.setItem("datos", JSON.stringify(arr));
             arr.forEach(item =>  {
                 const il = document.createElement('il');
                 il.innerHTML = `
                     ${item} 
                     `;
                 lista.appendChild(il);
-                })
-        })
+                });
+            item.value = '';
+            
+        });
 
-        agregar.addEventListener("keydown", function(event){
-            if(event.key("Enter")) {
-                 return addkey()
-            }
-        })
-        
-})
+    let itemInput = document.getElementById('item');
+
+    item.addEventListener('keydown', function(event){
+        if(event.code === "Enter") {
+            let item = itemInput.value;
+            arr.push(item);
+            localStorage.setItem("datos", JSON.stringify(arr));
+            lista.innerHTML = '';
+            arr.forEach(item =>  { 
+                const il = document.createElement('li');
+                il.textContent = item;
+                lista.appendChild(il);
+            });
+            itemInput.value = ''; 
+        }
+    });
+});
